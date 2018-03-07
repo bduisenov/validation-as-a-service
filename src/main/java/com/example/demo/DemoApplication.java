a2e8745b7bec53bd15c44ae383034b4c9308163b
Babur <bduisenov@gmail.com>
2018-03-07 16:38:21 +0100

    Updated to M4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +14,11 @@ import static java.lang.System.out;
 @SpringBootApplication
 public class DemoApplication {
 
+    @Autowired
+    private Function<String, Boolean> hasLowerCase;
+
     @Bean
-    public CommandLineRunner commandLineRunner(Map<String, Function<Object, Boolean>> validations) {
-        Function<Object, Boolean> hasLowerCase = validations.get("hasLowerCase");
+    public CommandLineRunner commandLineRunner() {
         return args -> {
             out.println("#################################### started ####################################");
             out.println(hasLowerCase.apply("true"));
