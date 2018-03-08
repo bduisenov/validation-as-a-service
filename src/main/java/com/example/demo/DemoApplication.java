b4abb20fbb7dce842ebf382181e1f72e641d1786
Babur <bduisenov@gmail.com>
2018-03-08 14:31:58 +0100

    Adjust rule

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.Resource;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -14,8 +15,8 @@ import static java.lang.System.out;
 @SpringBootApplication
 public class DemoApplication {
 
-    @Autowired
-    private Function<String, Optional<Boolean>> emailValidation;
+    @Resource
+    private Function<String, Optional<String>> emailValidation;
 
     @Bean
     public CommandLineRunner commandLineRunner() {
